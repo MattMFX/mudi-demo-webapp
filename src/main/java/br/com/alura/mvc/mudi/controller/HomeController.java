469b5bdc3c527e6d5e9b9325724ae745f4cd38cb
matheus0210 <matheus0210@hotmail.com>
2020-10-12 22:49:14 -0300

    html da home com controller da home

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -1,16 +1,35 @@
 package br.com.alura.mvc.mudi.controller;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import br.com.alura.mvc.mudi.model.Pedido;
+
 @Controller
 public class HomeController {
 
-	@GetMapping("/hello")
+	@GetMapping("/home")
 	public String helloWorld(Model model) {
-		model.addAttribute("nome", "World");
-		return "helloWorld";
+		Pedido pedido = new Pedido();
+		pedido.setNome("Echo dot");
+		pedido.setData(LocalDateTime.now());
+		pedido.setDescricao("Caixa de som com alexa integrada");
+		pedido.setValor(new BigDecimal(200.00));
+		pedido.setUrlProduto("https://www.amazon.com.br/Echo-Dot-3%C2%AA-Gera%C3%A7%C3%A3o-Cor-Preta/dp/B07PDHSJ1H/ref=sr_1_1?__mk_"
+				+ "pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=19VM998APKE9U&dchild=1&keywords=echo+dot&qid=1602550531&smid=A1ZZFT5FULY4LN&s"
+				+ "prefix=echo+do%2Caps%2C1863&sr=8-1");
+		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/61Rr8uxmREL._AC_SL1000_.jpg");
+		
+		List<Pedido> pedidos = Arrays.asList(pedido);
+		
+		model.addAttribute("pedidos", pedidos);
+		return "home";
 	}
 	
 }
---
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -0,0 +1,54 @@
+package br.com.alura.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Pedido {
+
+	private String nome;
+	private BigDecimal valor;
+	private LocalDateTime data;
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
+	
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+	public LocalDateTime getData() {
+		return data;
+	}
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+	
+}
