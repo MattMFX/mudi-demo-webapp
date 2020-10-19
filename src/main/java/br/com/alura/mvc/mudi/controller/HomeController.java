2e8f83f97fb145ef2bc1a6f5b89570a5d41b130a
matheus0210 <matheus0210@hotmail.com>
2020-10-19 04:07:21 -0300

    Revert "implemented spring data"
    This reverts commit 69a003eb5ffe3c978f95e6302d55c6ab1bcbbc3c.

--- src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/HomeController.java
@@ -2,23 +2,18 @@ package br.com.alura.mvc.mudi.controller;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import br.com.alura.mvc.mudi.dao.PedidoDao;
 import br.com.alura.mvc.mudi.model.Pedido;
 
 @Controller
 public class HomeController {
 
-
-	@Autowired
-	private PedidoDao pedidoDao;
-	
 	@GetMapping("/home")
 	public String home(Model model) {
 		Pedido pedido = new Pedido();
@@ -31,8 +26,7 @@ public class HomeController {
 				+ "prefix=echo+do%2Caps%2C1863&sr=8-1");
 		pedido.setUrlImagem("https://images-na.ssl-images-amazon.com/images/I/61Rr8uxmREL._AC_SL1000_.jpg");
 		
-		pedidoDao.createPedido(pedido);	
-		List<Pedido> pedidos = pedidoDao.readPedidos();
+		List<Pedido> pedidos = Arrays.asList(pedido);
 		
 		model.addAttribute("pedidos", pedidos);
 		return "home";
